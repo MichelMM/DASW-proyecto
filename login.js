b2689c9c23c4a01c49d5728cc21e0aa4b4d57060
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2019-11-22 07:34:17 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,43 @@
+let loginButton = document.getElementById("loginButton");
+
+loginButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    let objeto = {
+        mail: document.getElementById("loginEmail").value,
+        password: document.getElementById("loginPassword").value
+    }
+    loginDatos(objeto, function () {
+        let temp = JSON.parse(localStorage.usuario);
+        if (temp.length > 0) {
+            if (temp[0].password == document.getElementById("loginPassword").value) {
+                window.location.href = "/principal.html";
+            } else {
+                alert("Error, password o correo Incorrectos")
+            }
+        } else {
+            alert("Error, password o correo Incorrectos")
+        }
+
+
+    }, function () {
+        alert("No se pudo");
+    });
+
+});
+
+
+function loginDatos(datos, cbOk, cbErr) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://localhost:3000/usuario?mail=${datos.mail}`);
+    xhr.send([JSON.stringify(datos)]);
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+            alert(xhr.status + ': ' + xhr.statusText + '\n Error, correo o contraseña invalidos');
+            cbErr();
+        } else {
+            localStorage.setItem("usuario", xhr.response);
+            cbOk();
+            // 
+        }
+    };
+}
