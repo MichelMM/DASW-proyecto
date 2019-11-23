c0d528d79fcb459761574a8b021a1990d5f931a0
Ruvo99 <55141980+Ruvo99@users.noreply.github.com>
2019-11-22 19:25:30 -0600

    Add files via upload

--- registrarMaquina.js
+++ registrarMaquina.js
@@ -1,6 +1,29 @@
 let btnRegistrarMaquina = document.getElementById("registroMaquina");
+let formMaquinaria = document.getElementById("formMaquinaria");
+
+formMaquinaria.addEventListener("change", camposMaquinaria);
+
+function camposMaquinaria(){
+    let notValid = formMaquinaria.querySelectorAll(":invalid");
+    let valid = formMaquinaria.querySelectorAll(":valid");
+    console.log(formMaquinaria);
+    notValid.forEach(function (element){
+        element.style.border = '1px solid #ff0000';
+        formMaquinaria.querySelector('button').disabled = true;
+    });
+    if(notValid.length != 0){
+        formMaquinaria.querySelector('button').disabled = true;
+    }else{
+        formMaquinaria.querySelector('button').disabled = false;
+    }
+
+    valid.forEach(function(element){
+        element.style.border = '1px solid #ced4da';
+    });
+}
 
 btnRegistrarMaquina.addEventListener("click", function(event){
+    event.preventDefault();
     let objeto={
         id: document.getElementById("inputID").value,
         type: document.getElementById("inputMachine").value,
@@ -12,16 +35,16 @@ btnRegistrarMaquina.addEventListener("click", function(event){
 })
 
 function registrarMaquina(datos){
-    event.preventDefault();
     let xhr = new XMLHttpRequest();
     xhr.open('POST', "http://localhost:3000/Maquinaria");
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send([JSON.stringify(datos)]);
     xhr.onload = function () {
         if (xhr.status != 201) {
-            alert(xhr.status + ': ' + xhr.statusText  + 'Error, no se ha podido registrar la máquina');
+            alert('Error, no se ha podido registrar la máquina');
         } else {
-            alert(xhr.responseText+'\n La máquina ha sido registrada con éxito');
+            alert("La máquina ha sido registrada con éxito");
+            
         }
     };
 }
